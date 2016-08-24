ee589b2eea7ed8e1f4f88e5bac4eb3c447b64a8f
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-08-24 08:00:14 +0200

    restructured lesson

---
+++ sass_project/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var del = require('del');
+
+gulp.task('scss', function() {
+	return gulp.src(['src/scss/*.scss'])
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('copyFiles', function() {
+	return gulp.src(['src/**/*', '!src/scss', '!src/scss/**/*'])
+		.pipe(gulp.dest('dist/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/scss/*.scss', ['scss']);
+	gulp.watch('src/**/*', ['copyFiles']);
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		notify: false,
+		server: {
+			baseDir: './dist/',
+			index: 'index.html',
+			port: 3000
+		}
+	});
+});
+
+gulp.task('default', function() {
+	return runSequence(
+		'browser-sync',
+		'copyFiles',
+		'scss',
+		'watch'
+	);
+});
